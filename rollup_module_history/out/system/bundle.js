3bd8e4f4ee09cd8734b7bb3c56e896be3a2a7186
bxliao <407077499@qq.com>
2022-07-05 14:44:53 +0800

    前端模块发展史

---
+++ rollup_module_history/out/system/bundle.js
@@ -0,0 +1,21 @@
+System.register(['lodash'], (function (exports) {
+  'use strict';
+  var repeat;
+  return {
+    setters: [function (module) {
+      repeat = module.repeat;
+    }],
+    execute: (function () {
+
+      var answer = 42;
+
+      const printAnswer = exports('printAnswer', () => {
+        // 打印
+        console.log(`the answer is ${answer}`);
+        // 测试 lodash 的能力，打印42个1
+        console.log(repeat("1", answer));
+      });
+
+    })
+  };
+}));
